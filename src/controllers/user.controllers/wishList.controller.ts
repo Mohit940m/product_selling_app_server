da6537d74d2e5dc46cc11e101d777cd0073d6f14
Mohit Saha Chowdhdhury <sahachowdhurymohit@gmail.com>
2026-01-23 02:36:43 +0530

    feat(wishlist): add get wishlist API with controller and route
    - Added getWishList controller to fetch user wishlisted products
    - Created route for retrieving wishlist data
    - Integrated user-based filtering for wishlist items
    - Ensured optimized queries using indexed userId and productId

--- src/controllers/user.controllers/wishList.controller.ts
+++ src/controllers/user.controllers/wishList.controller.ts
@@ -42,7 +42,36 @@ const addProductToWishList = async (req: AuthRequest, res: Response) => {
     }
 };  
 
+const getWishList = async (req: AuthRequest, res: Response) => {
+    try {
+        if (!req.user) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized. User not found."
+            });
+        }
+        const userId = req.user._id;
+
+        const wishList = await WishList.find({ userId })
+            .select("userId productId addedAt")
+            .populate('productId', 'name price category images isFeatured');
+
+        return res.status(200).json({
+            success: true,
+            message: "Wishlist fetched successfully",
+            data: wishList
+        });
+    } catch (error: any) {
+        console.error("Get WishList Error:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            error: error.message
+        });
+    }
+};
 
 export {
     addProductToWishList,
+    getWishList,
 };
--- src/routes/user.routes/wishList.routes.ts
+++ src/routes/user.routes/wishList.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { 
     addProductToWishList,
+    getWishList,
 } from '../../controllers/user.controllers/wishList.controller.js';
 import { authenticateUser } from '../../auth/auth.middleware.js';
 
 const router = Router();
 
 router.post('/add', authenticateUser, addProductToWishList);
+router.get('/', authenticateUser, getWishList);
 
 export default router;
 export {};
